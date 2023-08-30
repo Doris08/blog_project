44250df9d6ef5de1372412ec2be481fd7b92b916
Doris08 <akhino44@gmail.com>
2023-08-30 00:38:13 -0600

    cambiando el load en rutas y modelos Post

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->get()
     ]);
 });
 
@@ -37,7 +37,7 @@ Route::get('posts/{post}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author'])
+        'posts' => $category->posts
     ]);
 
 });
@@ -46,7 +46,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
 Route::get('authors/{author:username}', function (User $author) {
 
     return view('posts', [
-        'posts' => $author->post->load(['category', 'author'])
+        'posts' => $author->post
     ]);
 
 });
