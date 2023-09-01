97f72488a94283e3f0f6499ad8cfda28b4871398
Doris08 <akhino44@gmail.com>
2023-09-01 16:14:31 -0600

    Agregando estilos y dinamismo con tailwind y AlpineJS

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,12 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
-});
+})->name('home');
 
-Route::get('posts/{post}', function (Post $post) {
+Route::get('posts/{post:slug}', function (Post $post) {
 
     return view('post', [
         'post' => $post
@@ -37,16 +38,19 @@ Route::get('posts/{post}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 
-});
+})->name('category');
 
 
 Route::get('authors/{author:username}', function (User $author) {
 
     return view('posts', [
-        'posts' => $author->post
+        'posts' => $author->post,
+        'categories' => Category::all()
     ]);
 
 });
