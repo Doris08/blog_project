b2df10eeb7cdc1cef19754f5e7c508d1549ed469
Doris08 <akhino44@gmail.com>
2023-09-04 18:26:42 -0600

    Agregando filtros para categoria, otros y por autores

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,6 +15,33 @@ class Post extends Model
 
     //protected $fillable = ['title', 'excerpt', 'body', 'id'];
 
+    //Creación de Función Filter
+    public function scopeFilter($query, array $filters){
+
+        $query->when($filters['search'] ?? false, function($query, $search){ // También se puede usar arrow functions fn($query, $search)=>$query
+            $query->where(fn()=>
+                $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%')
+            );
+        });
+        $query->when($filters['category'] ?? false, fn($query, $category)=> // Forma usando arrow function
+            $query->whereHas('category', fn($query) => 
+                $query->where('slug', $category))    
+        );  
+        $query->when($filters['author'] ?? false, fn($query, $author)=> // Forma usando arrow function
+            $query->whereHas('author', fn($query) => 
+                $query->where('username', $author))    
+        );  
+        
+        /*$query
+            ->whereExists(fn($query)=>
+                $query->from('categories')
+                    ->whereColumn('categories.id', 'posts.category_id')
+                    ->where('categories.slug', $category))*/
+     
+    }
+
     public function Category(){
 
         return $this->belongsTo(Category::class);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -17,47 +18,27 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-    $posts = Post::latest();
-
-    if(request('search')){
-        $posts
-        ->where('title', 'like', '%' . request('search') . '%')
-        ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
-    return view('posts', [
-        'posts' => $posts->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-
-    return view('post', [
-        'post' => $post
-    ]);
-
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 
 
-Route::get('categories/{category:slug}', function (Category $category) {
+/*Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
         'posts' => $category->posts,
         'currentCategory' => $category,
-        'categories' => Category::all()
+        'categories' => Category::get()
     ]);
 
-})->name('category');
+})->name('category');*/
 
 
-Route::get('authors/{author:username}', function (User $author) {
+/*Route::get('authors/{author:username}', function (User $author) {
 
-    return view('posts', [
-        'posts' => $author->post,
-        'categories' => Category::all()
+    return view('posts.index', [
+        'posts' => $author->post
     ]);
 
-});
+});*/
